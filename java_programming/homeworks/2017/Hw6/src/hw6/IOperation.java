b992fd2e4c92143d062f65d4fe28a556385c1610
auyu <datealive102164@gmail.com>
2021-08-19 00:57:01 +0800

    Add F722700: Java programming

--- java_programming/homeworks/2017/Hw6/src/hw6/IOperation.java
+++
@@ -1,6 +0,0 @@
-package hw6;
-
-public interface IOperation {
-	public abstract String perform(String num1,String num2);
-
-}
--- java_programming/homeworks/2019/hw6/src/hw6/IOperation.java
+++
@@ -1,5 +0,0 @@
-package hw6;
-
-public interface IOperation {
-	String perform(String num1, String num2);
-}
